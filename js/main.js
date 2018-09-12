352262daf242534c81d35a286d1dc7752a675fdb
Djrun <aghazadajeyran@gmail.com>
2018-09-12 18:27:37 +0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,71 @@
+
+    (function() {
+        var burger = document.querySelector('.burger');
+        var nav = document.querySelector('#' + burger.dataset.target);
+                
+        burger.addEventListener('click', function() {
+            burger.classList.toggle('is-active');
+            nav.classList.toggle('is-active');
+        });
+    })();
+    
+      $(document).ready(function(){
+        $(function(){
+            $(document).on( 'scroll', function(){
+                if ($(window).scrollTop() > 100) {
+                    $('.scroll-top-wrapper').addClass('show');
+                } else {
+                    $('.scroll-top-wrapper').removeClass('show');
+                }
+            });
+            $('.scroll-top-wrapper').on('click', scrollToTop);
+        });
+      function scrollToTop() {
+          verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+          element = $('body');
+          offset = element.offset();
+          offsetTop = offset.top;
+          $('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+      }
+      });
+        $('a[href*="#"]')
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function(event) {
+        if (
+            location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+            && 
+            location.hostname == this.hostname
+        ) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+             event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000, function() {
+                var $target = $(target);
+                $target.focus();
+                if ($target.is(":focus")) { 
+                return false;
+                } else {
+                $target.attr('tabindex','-1'); 
+                $target.focus(); 
+                };
+            });
+            }
+        }
+        });
+    
+    
+       $(function() {
+            $('a[href*=#]').on('click', function(e) {
+                e.preventDefault();
+                $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+            });
+        });
+  
+            $(document).ready(function() {
+                var carousels = bulmaCarousel.attach();
+                CarouselInstance.on('carousel:ready', function(item) {});
+            });
